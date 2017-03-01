8bdf2ae0604e4a4133dda0ea18240cdc842b76c1
kay <kay.kim@10gen.com>
2017-03-01 14:02:56 -0500

    Add clarifications to action requested in stubs

--- examples/ruby/query-top-level.rb
+++ examples/ruby/query-top-level.rb
@@ -2,6 +2,16 @@
 # https://docs-mongodbcom-staging.corp.mongodb.com/jdestefano/DOCSP-364/tutorial/insert-one.html
 
 
+# **Action Requested**
+#
+# Please provide equivalent examples in your driver language.
+#
+# - If the examples are not relevant for your driver, omit but leave comment as to why,
+# such as "key order is not guaranteed, however, you can enforce absolute key order via :some link: or something.
+# This way we can display that instead.
+#
+# - Include any additional comments that would be relevant for people using the driver for that example
+#
 
 # 0. Insert documents that we'll query
 #
