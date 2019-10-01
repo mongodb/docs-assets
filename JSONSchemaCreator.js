f6dff9e5b641ec6ae2285eb4b7eb8e8876b2a348
Chris Cho <chris.cho@10gen.com>
2019-10-01 14:13:20 -0400

    added JSONSchemaCreator javascript code

---
+++ JSONSchemaCreator.js
@@ -0,0 +1,48 @@
+function JSONSchemaCreator(keyId){
+    return {
+        bsonType: 'object',
+        encryptMetadata: {
+            keyId: keyId
+        },
+        properties: {
+        insurance: {
+            bsonType: "object",
+            properties: {
+            policyNumber: {
+                encrypt: {
+                bsonType: "int",
+                algorithm: "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic"
+                }
+            }
+            }
+        },
+        medicalRecords: {
+            encrypt: {
+                bsonType: "array",
+                algorithm: "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
+            }
+        },
+        bloodType: {
+            encrypt: {
+                bsonType: "string",
+                algorithm: "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
+            }
+        },
+        ssn: {
+            encrypt: {
+            bsonType: 'int',
+            algorithm: 'AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic',
+            }
+        },
+        mobile: {
+            encrypt: {
+            bsonType: 'string',
+            algorithm: 'AEAD_AES_256_CBC_HMAC_SHA_512-Random',
+            }
+        }
+        }
+    }
+};
+
+const patientSchema = JSONSchemaCreator("<paste_your_key_id_here>"); // replace the "paste_your_key_id_here" with your data key id
+console.log(JSON.stringify(patientSchema));
