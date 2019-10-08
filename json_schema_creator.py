4f012fc0ab4a0e02b02fa79425c9358521c120d2
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-10-08 15:58:21 -0400

    Create json_schema_creator.py

---
+++ json_schema_creator.py
@@ -0,0 +1,46 @@
+def json_schema_creator(keyId):
+    return {
+        'bsonType': 'object',
+        'encryptMetadata': {
+            'keyId': keyId
+        },
+        'properties': {
+        'insurance': {
+            'bsonType': "object",
+            'properties': {
+            'policyNumber': {
+                'encrypt': {
+                'bsonType': "int",
+                'algorithm': "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic"
+                }
+            }
+            }
+        },
+        'medicalRecords': {
+            'encrypt': {
+                'bsonType': "array",
+                'algorithm': "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
+            }
+        },
+        'bloodType': {
+            'encrypt': {
+                'bsonType': "string",
+                'algorithm': "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
+            }
+        },
+        'ssn': {
+            'encrypt': {
+            'bsonType': 'int',
+            'algorithm': 'AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic',
+            }
+        },
+        'mobile': {
+            'encrypt': {
+            'bsonType': 'string',
+            'algorithm': 'AEAD_AES_256_CBC_HMAC_SHA_512-Random',
+            }
+        }
+        }
+    }
+
+print("<paste_your_data_key_id>"); # replace the "paste_your_data_key_id" with your data key id
