7b36377784648fa93724eaf776567887586420d3
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-10-10 17:21:01 -0400

    ran autopep8 and flake8

--- data_encryption_key_generator.py
+++ data_encryption_key_generator.py
@@ -4,11 +4,9 @@ from pymongo.encryption import ClientEncryption
 import base64
 from bson.codec_options import CodecOptions
 import os
-from bson.binary import (Binary,
-                         JAVA_LEGACY,
-                         STANDARD,
-                         UUID_SUBTYPE)
-import uuid                         
+from bson import binary
+from bson.binary import (STANDARD)
+import uuid
 
 OPTS = CodecOptions(uuid_representation=STANDARD)
 connection_string = "mongodb://localhost:27017"
@@ -20,27 +18,27 @@ f = open(path, "wb")
 f.write(file_bytes.strip())
 f.close()
 
-from bson import binary
 
-path = "./master-key.txt";
+path = "./master-key.txt"
 local_master_key = binary.Binary(open(path, "rb").read(96))
 
 kms_providers = {
-  "local": {
-    "key": local_master_key,
-  },
+    "local": {
+        "key": local_master_key,
+    },
 }
+
 
 def data_encryption_key_generator():
     fle_opts = AutoEncryptionOpts(
-    kms_providers,
-    key_vault_namespace,
-    mongocryptd_bypass_spawn = True
+        kms_providers,
+        key_vault_namespace,
+        mongocryptd_bypass_spawn=True
     )
 
     client = MongoClient(
         connection_string,
-        auto_encryption_opts = fle_opts
+        auto_encryption_opts=fle_opts
     )
 
     client_encryption = ClientEncryption(
@@ -54,5 +52,6 @@ def data_encryption_key_generator():
     base_64_data_key_id = base64.b64encode(data_key)
     print("DataKeyId [UUID]: ", uuid_data_key_id)
     print("DataKeyId [base64]: ", base_64_data_key_id)
+
 
 data_encryption_key_generator()
