f187641635303f50d26c700d6110015b251a4574
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-10-15 09:28:39 -0400

    Ran linter on py file

--- json_schema_creator.py
+++ json_schema_creator.py
@@ -5,44 +5,45 @@ def json_schema_creator(key_id):
             'keyId': key_id
         },
         'properties': {
-        'insurance': {
-            'bsonType': "object",
-            'properties': {
-            'policyNumber': {
+            'insurance': {
+                'bsonType': "object",
+                'properties': {
+                    'policyNumber': {
+                        'encrypt': {
+                            'bsonType': "int",
+                            'algorithm':
+                            "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic"
+                        }
+                    }
+                }
+            },
+            'medicalRecords': {
                 'encrypt': {
-                'bsonType': "int",
-                'algorithm': "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic"
+                    'bsonType': "array",
+                    'algorithm': "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
+                }
+            },
+            'bloodType': {
+                'encrypt': {
+                    'bsonType': "string",
+                    'algorithm': "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
+                }
+            },
+            'ssn': {
+                'encrypt': {
+                    'bsonType': 'int',
+                    'algorithm': 'AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic',
+                }
+            },
+            'mobile': {
+                'encrypt': {
+                    'bsonType': 'string',
+                    'algorithm': 'AEAD_AES_256_CBC_HMAC_SHA_512-Random',
                 }
             }
-            }
-        },
-        'medicalRecords': {
-            'encrypt': {
-                'bsonType': "array",
-                'algorithm': "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
-            }
-        },
-        'bloodType': {
-            'encrypt': {
-                'bsonType': "string",
-                'algorithm': "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
-            }
-        },
-        'ssn': {
-            'encrypt': {
-            'bsonType': 'int',
-            'algorithm': 'AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic',
-            }
-        },
-        'mobile': {
-            'encrypt': {
-            'bsonType': 'string',
-            'algorithm': 'AEAD_AES_256_CBC_HMAC_SHA_512-Random',
-            }
-        }
         }
     }
 
 
-print(json_schema_creator("<paste_your_data_key_id>")) # replace the "paste_your_data_key_id" with your data key id
-
+# replace the "paste_your_data_key_id" with your data key id
+print(json_schema_creator("<paste_your_data_key_id>"))
