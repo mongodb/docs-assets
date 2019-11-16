6896a0be36e4afa14b6b38af2acadb7a59161586
Chris Cho <chris.cho@10gen.com>
2019-11-16 12:24:41 -0500

    java example - remove dependency to json library, fix schema

--- JSONSchemaCreator.java
+++ JSONSchemaCreator.java
@@ -1,60 +1,54 @@
-package com.example.fle;
+package com.example.csfle;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 
-public class JSONSchemaCreator {
+public class JSONSChemaCreator {
     public static final String DETERMINISTIC_ENCRYPTION_TYPE = "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic";
     public static final String RANDOM_ENCRYPTION_TYPE = "AEAD_AES_256_CBC_HMAC_SHA_512-Random";
 
-    public static JSONObject buildEncryptedField(String keyId, String bsonType, Boolean isDeterministic) {
-        return new JSONObject()
-                .put("encrypt", new JSONObject()
-                        .put("bsonType", bsonType)
-                        .put("algorithm", (isDeterministic) ? DETERMINISTIC_ENCRYPTION_TYPE : RANDOM_ENCRYPTION_TYPE)
-                        );
+    public static DBObject buildEncryptedField(String keyId, String bsonType, Boolean isDeterministic) {
+        return new BasicDBObject().
+                append("encrypt", new BasicDBObject()
+                        .append("bsonType", bsonType)
+                        .append("algorithm", 
+                                (isDeterministic) ? DETERMINISTIC_ENCRYPTION_TYPE : RANDOM_ENCRYPTION_TYPE));
     }
 
-    public static JSONObject buildEncryptMetadata(String keyId) {
-        return new JSONObject()
-                .put("keyId", new JSONArray()
-                        .put(new JSONObject()
-                                .put("$binary", new JSONObject()
-                                        .put("base64", keyId)
-                                        .put("subType",  "04")
-                                        )
-                                )
-                        );
+    public static DBObject buildEncryptMetadata(String keyId) {
+        BasicDBList keyIds = new BasicDBList();
+        keyIds.add(new BasicDBObject()
+                .append("$binary", new BasicDBObject()
+                        .append("base64", keyId)
+                        .append("subType", "04")));
+        return new BasicDBObject().append("keyId", keyIds);
     }
 
-    public static JSONObject createJSONSchema(String keyId) {
-        return new JSONObject()
-                .put("bsonType", "object")
-                .put("encryptMetadata", buildEncryptMetadata(keyId))
-                .put("properties", new JSONObject()
-                        .put("ssn", buildEncryptedField(keyId, "int", true))
-                        .put("bloodType", buildEncryptedField(keyId, "int", false))
-                        .put("medicalRecords", buildEncryptedField(keyId, "array", false))
-                        .put("insurance", new JSONObject()
-                                .put("properties", new JSONObject()
-                                        .put("policyNumber", buildEncryptedField(keyId, "int", true))
-                                        )
-                                )
-                        );
+    public static DBObject createJSONSchema(String keyId) {
+        return new BasicDBObject().append("bsonType", "object").append("encryptMetadata", buildEncryptMetadata(keyId))
+                .append("properties", new BasicDBObject()
+                        .append("ssn", buildEncryptedField(keyId, "int", true))
+                        .append("bloodType", buildEncryptedField(keyId, "string", false))
+                        .append("medicalRecords", buildEncryptedField(keyId, "array", false))
+                        .append("insurance", new BasicDBObject()
+                                .append("bsonType", "object")
+                                .append("properties",
+                                        new BasicDBObject().append("policyNumber", buildEncryptedField(keyId, "int", true)))));
     }
-
 
     public static void main(String[] args) throws IOException {
-        JSONObject schema = createJSONSchema("<paste_your_key_id_here>"); // replace the "paste_your_key_id_here" with your data key id
+        String schema = createJSONSchema("<your_key_id>").toString(); // replace "your_key_id" with your base64 data encryption key id
 
         String path = "fle-schema.json";
         try (FileOutputStream stream = new FileOutputStream(path)) {
-            stream.write(schema.toString().getBytes());
+            stream.write(schema.getBytes());
         }
 
         System.out.println(schema);
     }
 }
+
