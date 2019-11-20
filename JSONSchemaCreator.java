a243c9c4b03721ee4cd7e80afb291c7e28c62119
Chris Cho <chris.cho@10gen.com>
2019-11-20 17:52:44 -0500

    use Document instead of DBObject

--- JSONSchemaCreator.java
+++ JSONSchemaCreator.java
@@ -2,42 +2,42 @@ package com.example.fle;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.mongodb.BasicDBList;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
+import org.bson.Document;
 
 public class JSONSChemaCreator {
     public static final String DETERMINISTIC_ENCRYPTION_TYPE = "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic";
     public static final String RANDOM_ENCRYPTION_TYPE = "AEAD_AES_256_CBC_HMAC_SHA_512-Random";
 
-    public static DBObject buildEncryptedField(String keyId, String bsonType, Boolean isDeterministic) {
-        return new BasicDBObject().
-                append("encrypt", new BasicDBObject()
+    public static Document buildEncryptedField(String keyId, String bsonType, Boolean isDeterministic) {
+        return new Document().
+                append("encrypt", new Document()
                         .append("bsonType", bsonType)
                         .append("algorithm",
                                 (isDeterministic) ? DETERMINISTIC_ENCRYPTION_TYPE : RANDOM_ENCRYPTION_TYPE));
     }
 
-    public static DBObject buildEncryptMetadata(String keyId) {
-        BasicDBList keyIds = new BasicDBList();
-        keyIds.add(new BasicDBObject()
-                .append("$binary", new BasicDBObject()
+    public static Document buildEncryptMetadata(String keyId) {
+        List<Document> keyIds = new ArrayList<Document>();
+        keyIds.add(new Document()
+                .append("$binary", new Document()
                         .append("base64", keyId)
                         .append("subType", "04")));
-        return new BasicDBObject().append("keyId", keyIds);
+        return new Document().append("keyId", keyIds);
     }
 
-    public static DBObject createJSONSchema(String keyId) {
-        return new BasicDBObject().append("bsonType", "object").append("encryptMetadata", buildEncryptMetadata(keyId))
-                .append("properties", new BasicDBObject()
+    public static Document createJSONSchema(String keyId) {
+        return new Document().append("bsonType", "object").append("encryptMetadata", buildEncryptMetadata(keyId))
+                .append("properties", new Document()
                         .append("ssn", buildEncryptedField(keyId, "int", true))
                         .append("bloodType", buildEncryptedField(keyId, "string", false))
                         .append("medicalRecords", buildEncryptedField(keyId, "array", false))
-                        .append("insurance", new BasicDBObject()
+                        .append("insurance", new Document()
                                 .append("bsonType", "object")
                                 .append("properties",
-                                        new BasicDBObject().append("policyNumber", buildEncryptedField(keyId, "int", true)))));
+                                        new Document().append("policyNumber", buildEncryptedField(keyId, "int", true)))));
     }
 
     public static void main(String[] args) throws IOException {
@@ -51,4 +51,3 @@ public class JSONSChemaCreator {
         System.out.println(schema);
     }
 }
-
